e2d1d57bce70693728b1344897d6424cb032aaca
priiyajha <jhapriiyaa2104@gmail.com>
2025-08-17 10:44:56 +0530

    Fixed errors and added user auth

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Firebase Config
+
+//Firebase Config
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyDZaM8Pmmw-KwaWkuhfdBhf8xDaxEsdZMM",
+  authDomain: "omniplex-a9568.firebaseapp.com",
+  projectId: "omniplex-a9568",
+  storageBucket: "omniplex-a9568.firebasestorage.app",
+  messagingSenderId: "873852565111",
+  appId: "1:873852565111:web:546c2a09c834b2b4d1f872",
+  measurementId: "G-LX1HK5VV2P"
 };
-
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 const storage = getStorage(app);
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,26 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+//
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//     ],
+//   },
+//   compiler: { removeConsole: process.env.NODE_ENV === "production", },
+//
+// };
+//
+// export default nextConfig;
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
+  webpack: (config) => {
+    // Don’t let undici be bundled in browser build
+    config.resolve.alias["undici"] = false;
+    return config;
   },
 };
 
