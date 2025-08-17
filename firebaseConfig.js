d9ec44a7a6596abc5e9f51aaa842a83dfeccb26d
priiyajha <jhapriiyaa2104@gmail.com>
2025-08-17 10:58:34 +0530

    Resolved Conflicts

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 //Firebase Config
 export const firebaseConfig = {
-  apiKey: "AIzaSyDZaM8Pmmw-KwaWkuhfdBhf8xDaxEsdZMM",
-  authDomain: "omniplex-a9568.firebaseapp.com",
-  projectId: "omniplex-a9568",
-  storageBucket: "omniplex-a9568.firebasestorage.app",
-  messagingSenderId: "873852565111",
-  appId: "1:873852565111:web:546c2a09c834b2b4d1f872",
-  measurementId: "G-LX1HK5VV2P"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
